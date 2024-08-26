82aaaffb9b5da7f70eb6e29fd0bc9a091d2fc8a3
Lucassmatheu <158113851+Lucassmatheu@users.noreply.github.com>
2024-08-25 23:32:38 -0300

    Atulaização

--- Simulador-NIVEL-2-desafio-manutencao-LucasMatheus/CodigosFontes/index.js
+++ Simulador-NIVEL-2-desafio-manutencao-LucasMatheus/CodigosFontes/index.js
@@ -1,7 +1,6 @@
 // História de Usuário 3: Apresentação de Conteúdo
 // Adiciona um evento ao botão que dispara uma função ao ser clicado
 document.getElementById('ctaButton').addEventListener('click',function()
- // Exibe um alerta na tela com a mensagem "Botão clicado!"
 { alert('Botão clicado!');
 
 });
