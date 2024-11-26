e5f18144c249cf615f6d616da1a94ffedf9b0bd8
Lucassmatheu <Lucas_matheus_06@hotmail.com>
2024-11-26 16:11:08 -0300

    atualização

--- Simuladores antigos/Simulador Recente nivel 4 Lucas Matheus/SimuladorDeSeguros/SimuladorDeSeguros/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace SimuladorDeSeguros.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- Simuladores antigos/Simulador-NIVEL-2-desafio-manutencao-LucasMatheus/CodigosFontes/index.js
+++
@@ -1,6 +0,0 @@
-// História de Usuário 3: Apresentação de Conteúdo
-// Adiciona um evento ao botão que dispara uma função ao ser clicado
-document.getElementById('ctaButton').addEventListener('click',function()
-{ alert('Botão clicado!');
-
-});
